6f1ea8ab4042d658f70faedb1fab65053c041970
Goteck1 <161943975+Goteck1@users.noreply.github.com>
2025-05-22 12:43:43 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Asegura que el directorio raíz del proyecto esté en sys.path
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+
 from src import create_app
 
 app = create_app()
