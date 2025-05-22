726ce59e7062771876ae834658ab069f33950647
Goteck1 <161943975+Goteck1@users.noreply.github.com>
2025-05-22 13:11:16 +0100

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
