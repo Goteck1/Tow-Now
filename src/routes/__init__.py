519cc993a937920b6f6248974a2014f09d3c0791
Goteck1 <161943975+Goteck1@users.noreply.github.com>
2025-05-22 17:00:12 +0100

    Update __init__.py

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -1,1 +1,14 @@
+from .main import main_bp
+from .user import user_bp
+from .admin import admin_bp
+from .service_assignment import service_assignment_bp
+from .client_notifications import client_notifications_bp
+
+__all__ = [
+    "main_bp",
+    "user_bp",
+    "admin_bp",
+    "service_assignment_bp",
+    "client_notifications_bp",
+]
 
